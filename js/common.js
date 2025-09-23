16443c2c3a7ffedc54f4354abfeccffe95ac697d
Viktoria Zaharova <zaharova181114@gmail.com>
2025-09-24 01:17:45 +0300

    all

--- js/common.js
+++ js/common.js
@@ -12,16 +12,51 @@ if (screen.width > 992) {
 	});
 }
 
+// mobile menu
+const mainNavigation = document.querySelector(".main-navigation");
+const toggler = mainNavigation.querySelector(".navbar-toggler");
+
+const openSideNav = () => mainNavigation.classList.add("active");
+const closeSideNav = () => mainNavigation.classList.remove("active");
+toggler.addEventListener("click", openSideNav);
+
+if (toggler) {
+	const body = document.querySelector('body');
+	const close = document.querySelector('.sidebar-close');
+	toggler.addEventListener("click", function (e) {
+		body.classList.add('active');
+	});
+	close.addEventListener("click", function (e) {
+		body.classList.remove('active');
+		mainNavigation.classList.remove('active');
+	});
+};
+
 $('.reviews-slider').slick({
 	slidesToShow: 3,
 	slidesToScroll: 1,
 	arrows: false,
 	dots: true,
 	autoplay: true,
-	responsive: [	
-		{ breakpoint: 992, settings: { slidesToShow: 2 } },
-		{ breakpoint: 576, settings: { slidesToShow: 1 } }
-	]
+	responsive: [
+    {
+      breakpoint: 992,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 576,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
 });
 
 
